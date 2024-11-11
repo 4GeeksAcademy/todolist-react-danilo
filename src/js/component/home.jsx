5c85763c38cb50d6ae44b0ff2c10335b3cbdd1a6
DaniloYP <daniloyp@gmail.com>
2024-11-11 21:31:20 +0000

    todolist 1 danilo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [item, setItem] = useState("")
+	const [toDos, setToDos] = useState([])
+	console.log(toDos)
+
+	const handleEnter = (e) => {
+		if (e.key === "Enter") {
+			setToDos([...toDos,item])
+			setItem("")
+		}
+	}
+
+	const handleDelete = (i) => {
+		
+		
+		setToDos(toDos.filter((_, index) => index !== i))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div classNameName="container">
+			<h1>Todos</h1>
+			<div>
+				<ul>
+					<li><input type ="text" placeholder="Agregar nueva tarea" onChange={(e) => setItem(e.target.value)} value ={item} onKeyDown={(e) => handleEnter(e)}/></li>
+					{toDos && toDos.map((item, index) => (
+						<li key={index}> {item} <i className="fas fa-times" onClick={() => handleDelete(index)}></i></li>
+					))}
+					<li> {toDos.length} items faltantes</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
